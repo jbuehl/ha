1ead66f7de96da45d55e24702a70ea23bb0e4023
Joe Buehl <joe@thebuehls.com>
2015-02-16 07:51:38 -0800

    GPIO improvements

--- ha/GPIOInterface.py
+++ ha/GPIOInterface.py
@@ -5,19 +5,19 @@ import RPIO as gpio
 
 class GPIOInterface(HAInterface):
     # MCP23017 I2C I/O expander
-    baseAddr = 0x20
-    nControls = 1
     ioDirRegister = 0x00
     ioPullupRegister = 0x0c
     ioRegister = 0x12
     # direct GPIO
-    pins = [12, 16, 18, 22, 15, 13, 11, 7, \    # A/B
-            32, 36, 38, 40, 37, 35, 33, 31]     # B+
+    pins = [12, 16, 18, 22, 15, 13, 11, 7]   # A/B
+#            32, 36, 38, 40, 37, 35, 33, 31]     # B+
     
-    def __init__(self, name, I2CInterface=None):
+    def __init__(self, name, I2CInterface=None, addr=0x20, inOut=[0x00, 0x00]):
         self.name = name
         if I2CInterface:
             HAInterface.__init__(self, name, I2CInterface)
+            self.addr = addr
+            self.inOut = inOut
         else:
             self.interface = None
             gpio.setwarnings(False)
@@ -28,35 +28,32 @@ class GPIOInterface(HAInterface):
     
     def start(self):
         if self.interface:
-            for control in range(GPIOInterface.nControls):
-                addr = GPIOInterface.baseAddr + control
-#                self.interface.write((addr, GPIOInterface.ioDirRegister), 0xff)     # bank 0 input
-#                self.interface.write((addr, GPIOInterface.ioPullupRegister), 0xff)  # bank 0 pullup
-                self.interface.write((addr, GPIOInterface.ioDirRegister), 0x00)   # bank 0 output
-#                self.interface.write((addr, GPIOInterface.ioDirRegister+1), 0xff)     # bank 1 input
-#                self.interface.write((addr, GPIOInterface.ioPullupRegister+1), 0xff)  # bank 1 pullup
-                self.interface.write((addr, GPIOInterface.ioDirRegister+1), 0x00)   # bank 1 output
+            for bank in range(2):
+                self.interface.write((self.addr, GPIOInterface.ioDirRegister+bank), self.inOut[bank])     # direction
+                self.interface.write((self.addr, GPIOInterface.ioPullupRegister+bank), self.inOut[bank])  # pullup
+        else:
+            pass
 
-    def read(self, theAddr):
+    def read(self, gpioAddr):
         if self.interface:
-            byte = self.interface.read((GPIOInterface.baseAddr+theAddr.control, GPIOInterface.ioRegister+theAddr.bank))
-            return (byte >> theAddr.start) & ((1<<theAddr.width)-1)
+            byte = self.interface.read((self.addr, GPIOInterface.ioRegister+gpioAddr.bank))
+            return (byte >> gpioAddr.start) & ((1<<gpioAddr.width)-1)
         else:
             return 0
 
-    def write(self, theAddr, theValue):
+    def write(self, gpioAddr, value):
         if self.interface:
-            byte = self.interface.read((GPIOInterface.baseAddr+theAddr.control, GPIOInterface.ioRegister+theAddr.bank))
-            mask = ((1<<theAddr.width)-1)<<theAddr.start
-            byte = (byte & (~mask)) | ((theValue << theAddr.start) & mask)
-            self.interface.write((GPIOInterface.baseAddr+theAddr.control, GPIOInterface.ioRegister+theAddr.bank), byte)
+            byte = self.interface.read((self.addr, GPIOInterface.ioRegister+gpioAddr.bank))
+            mask = ((1<<gpioAddr.width)-1)<<gpioAddr.start
+            byte = (byte & (~mask)) | ((value << gpioAddr.start) & mask)
+            self.interface.write((self.addr, GPIOInterface.ioRegister+gpioAddr.bank), byte)
         else:
-        		gpio.output(GPIOInterface.pins[theAddr.start], theValue)
+        		gpio.output(GPIOInterface.pins[gpioAddr.start], value)
             
         
 class GPIOAddr(object):
-    def __init__(self, control, bank, start, width):
-        self.control = control  # the I/O chip
+    def __init__(self, control, bank, start, width=1):
+        self.control = control  # the I/O chip (deprecated, use a separate instance of GPIOInterface per chip)
         self.bank = bank        # the 8 bit bank of I/O pins within the chip
         self.start = start      # the starting pin
         self.width = width      # the number of pins
