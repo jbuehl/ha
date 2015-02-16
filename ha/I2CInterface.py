9cd82254dee531b55d14d23dd5530b72d6a1af8a
Joe Buehl <joe@thebuehls.com>
2015-02-16 07:50:02 -0800

    Add i2c debug

--- ha/I2CInterface.py
+++ ha/I2CInterface.py
@@ -9,20 +9,24 @@ class HAI2CInterface(HAInterface):
 
     def read(self, theAddr):
         try:
+            if debugI2C: log(self.name, "readByte", theAddr)
             return self.bus.read_byte_data(*theAddr)
         except:
             return 0
 
     def readWord(self, theAddr):
         try:
+            if debugI2C: log(self.name, "readWord", theAddr)
             return self.bus.read_word_data(*theAddr)
         except:
             return 0
 
     def write(self, theAddr, theValue):
+        if debugI2C: log(self.name, "writeByte", theAddr, theValue)
         self.bus.write_byte_data(*theAddr+(theValue,))
 
     def writeWord(self, theAddr, theValue):
+        if debugI2C: log(self.name, "writeWord", theAddr, theValue)
         self.bus.write_word_data(*theAddr+(theValue,))
 
 
