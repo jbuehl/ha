adb1523b6f48612c8413cea0598928780963b292
Joe Buehl <joe@thebuehls.com>
2015-03-15 14:35:39 -0700

    Allow db table to be specified

--- ha/solarInterface.py
+++ ha/solarInterface.py
@@ -3,16 +3,16 @@ from ha.HAClasses import *
 class HASolarInterface(HAInterface):
     def __init__(self, theName, theInterface):
         HAInterface.__init__(self, theName, theInterface)
-        self.sql = """select %s(inverters.%s) from inverters,
-	            (select date, max(time) time, id from inverters
+        self.sql = """select %s(%s.%s) from %s,
+	            (select date, max(time) time, id from %s
 		            where date = curdate()
 		            group by id) max
-	            where inverters.date = max.date and inverters.time = max.time and inverters.id = max.id;	
+	            where %s.date = max.date and %s.time = max.time and %s.id = max.id;	
 	            """
 
-    def read(self, theAddr):
+    def read(self, addr):
         try:
-            sql = self.sql % (theAddr[1], theAddr[2])
+            sql = self.sql % (addr[1], addr[0], addr[2], addr[0], addr[0], addr[0], addr[0], addr[0])
             return self.interface.read(sql)
         except:
             return "-"
