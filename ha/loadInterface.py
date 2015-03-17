571a3f6e2ed9e812a5b5bcfdb244e0eb1e9d78e1
Joe Buehl <joe@thebuehls.com>
2015-03-16 20:14:11 -0700

    Change to use json

--- ha/loadInterface.py
+++ ha/loadInterface.py
@@ -1,28 +1,29 @@
 from ha.HAClasses import *
 
-class HALoadInterface(HAInterface):
-    def __init__(self, theName, theInterface):
-        HAInterface.__init__(self, theName, theInterface)
-        self.sql = """select loads.%s from loads,
-	                    (select max(time) time from loads where date = curdate()) max
-	                    where loads.date = curdate() and loads.time = max.time;	
-                        """
-        self.volts = {"lights":120,
-	                    "plugs":120,
-	                    "appl1":120,
-	                    "cooking":240,
-	                    "appl2":120,
-	                    "ac":240,
-	                    "pool":240,
-	                    "back":240,
-	                    }
+class LoadInterface(HAInterface):
+    def __init__(self, name, interface):
+        HAInterface.__init__(self, name, interface)
+#        self.sql = """select loads.%s from loads,
+#	                    (select max(time) time from loads where date = curdate()) max
+#	                    where loads.date = curdate() and loads.time = max.time;	
+#                        """
+        self.volts = {"Lights":120,
+	                  "Plugs":120,
+	                  "Appl1":120,
+	                  "Cooking":240,
+	                  "Appl2":120,
+	                  "Ac":240,
+	                  "Pool":240,
+	                  "Back":240,
+	                   }
 
-    def read(self, theAddr):
+    def read(self, addr):
         try:
-            table = theAddr[0]
-            sensor = theAddr[2].lower()
-            sql = self.sql % (sensor)
-            return self.interface.read(sql) * self.volts[sensor]
+#            table = theAddr[0]
+#            sensor = theAddr[2].lower()
+#            sql = self.sql % (sensor)
+#            return self.interface.read(sql) * self.volts[sensor]
+            return self.interface.read(addr) * self.volts[addr]
         except:
             return "-"
 
