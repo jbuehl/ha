7bf3e9abbd0c8610baa04753b9b9ef12d81d6c77
Joe Buehl <joe@thebuehls.com>
2015-03-30 21:40:23 -0700

    Tweak temp color algorithm

--- static/js/update.js
+++ static/js/update.js
@@ -11,40 +11,40 @@
                     pending = false;
                     });
                 var tempColor = function(temp) {        // set temp color based on temp value
-                    if (temp > 145){
-                        var red = 250;
+                    if (temp > 126){
+                        var red = 252;
                         var green = 0;
-                        var blue = 250;
+                        var blue = 252;
                         }
-                    else if (temp > 120){
-                        var red = 250;
+                    else if (temp > 105){
+                        var red = 252;
                         var green = 0;
-                        var blue = (temp-120)*10;
+                        var blue = (temp-105)*12;
                         }
-                    else if (temp > 95){
-                        var red = 250;
-                        var green = (120-temp)*10;
+                    else if (temp > 84){
+                        var red = 252;
+                        var green = (105-temp)*12;
                         var blue = 0;
                         }
-                    else if (temp > 70){
-                        var red = (temp-70)*10;
-                        var green = 250;
+                    else if (temp > 63){
+                        var red = (temp-63)*12;
+                        var green = 252;
                         var blue = 0;
                         }
-                    else if (temp > 45){
+                    else if (temp > 42){
                         var red = 0;
-                        var green = 250;
-                        var blue = (70-temp)*10;
+                        var green = 252;
+                        var blue = (63-temp)*12;
                         }
-                    else if (temp > 20){
+                    else if (temp > 21){
                         var red = 0;
-                        var green = (temp-20)*10;
-                        var blue = 250;
+                        var green = (temp-21)*12;
+                        var blue = 252;
                         }
                     else{
                         var red = 0;
                         var green = 0;
-                        var blue = 250;
+                        var blue = 252;
                         }
                     return 'rgb('+red.toString()+','+green.toString()+','+blue.toString()+')';
                     }
