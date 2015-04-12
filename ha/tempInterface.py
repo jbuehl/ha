061980ffea62189ca482c79289a24e64303d9e24
Joe Buehl <joe@thebuehls.com>
2015-04-11 18:42:36 -0700

    Sample once/sec and report average once/min

--- ha/tempInterface.py
+++ ha/tempInterface.py
@@ -7,12 +7,34 @@ class TempInterface(HAInterface):
         HAInterface.__init__(self, theName, theInterface)
 
     def start(self):
+        # data sampling thread
         def readData():
             debug('debugTemp', self.name, "readData started")
-            while running:
-                debug('debugTemp', self.name, "waiting", tempPollInterval)
-                time.sleep(tempPollInterval)
-                self.readData()
+            temp = self.readData()    # initialize states with a reading
+            samples = 1
+            lastMinute = -1
+            # sample once per second and update states with the average every minute
+            while True:
+                thisMinute = int(time.strftime("%M"))
+                debug('debugTemp', self.name, "thisMinute", thisMinute, "lastMinute", lastMinute)
+                if thisMinute != lastMinute:                    # start of a new minute
+                    for addr in self.sensorAddrs.keys():    # average the readings
+                        self.states[addr] = temp[addr]/samples
+                        temp[addr] = 0.0
+                    if self.event:
+                        self.event.set()
+                    debug('debugTemp', self.name, "state", self.states, "samples", samples)
+                    samples = 0
+                    lastMinute = thisMinute
+                sample = self.readData()            # take another sample
+                for addr in sample.keys():
+                    try:
+                        temp[addr] += sample[addr]
+                    except KeyError:                # a sensor was added during the sample period
+                        temp[addr] = sample[addr]
+                samples += 1
+                time.sleep(1)
+                
         readStatesThread = threading.Thread(target=readData)
         readStatesThread.start()
 
@@ -20,11 +42,12 @@ class TempInterface(HAInterface):
         debug('debugTemp', self.name, "read", addr)
         return self.states[addr]
 
+    # take a reading of all sensors
     def readData(self):
-        debug('debugTemp', self.name, "readData sensors", self.sensors, self.sensorAddrs)
-        for sensor in self.sensors.keys():
-            self.states[self.sensors[sensor].addr] = self.interface.read(self.sensors[sensor].addr)
-        debug('debugTemp', self.name, "readData states", self.states)
-        if self.event:
-            self.event.set()
+        temp = {}
+        for addr in self.sensorAddrs.keys():
+            temp[addr] = self.interface.read(addr)
+        debug('debugTemp', self.name, "readData", temp)
+        return temp
+        
 
