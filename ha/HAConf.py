fd3b43da77f3f0fcb604627dabf967bf01fefb5e
Joe Buehl <joe@thebuehls.com>
2015-04-18 14:41:12 -0700

    Add elevation

--- ha/HAConf.py
+++ ha/HAConf.py
@@ -13,6 +13,7 @@ sysLogging = True
 
 # Localization
 latLong = (34.1486, -118.3965)
+elevation = 620 # elevation in feet
 tempScale = "F"
 
 # Notification
@@ -36,74 +37,7 @@ debugRestGet = False
 debugRestPut = False
 debugRestStates = False
 
-# X10 interface
-#x10Device = "/dev/ttyUSB0"
-#debugLights = True
-
-# I2C and GPIO interfaces
-#debugI2C = False
-#debugGPIO = False
-
-# Data base interface
-#dbRetryInterval = 60
-#debugSql = False
-
-# AquaLink interface
-#aqualinkDevice = "/dev/aqualink"
-###aqualinkDevice = "/dev/ttyUSB0"
-###allButtonPanelAddr = '\x09'
-###monitorMode = False
-###aqualinkClock = True
-#debugData = False
-#debugRaw = False
-#debugProbe = False
-#debugAck = False
-#debugStatus = True
-#debugAction = True
-#debugMsg = False
-#debugTime = False
-
-# spa interface
-###spaTempTarget = 100
-###spaReadyNotifyNumbers = keyDir+"spaReadyNotifyNumbers"
-#debugSpaLight = True
-
-# Shades interface
-#debugShades = True
-
-# Web interface
-#webPort = 80
-#webRestPort = 7478
-#webUpdateInterval = 1
-#webUpdateStateChange = True
-#webLogging = False
-#debugHttp = False
-#debugWeb = True
-
 # Scheduler
-#debugEvent = True
-#debugSched = False
-
-# Pentair interface
-#pentairDevice = "/dev/pentair"
-###pentairDevice = "/dev/ttyUSB1"
-###pentairAddr = '\x60'
-###pentairSpeeds = [0, 1200, 1800, 2400, 3200]
-#debugPentairThread = False
-#debugPentairData = False
-
-# File interface
-#debugFile = True
-#debugFileThread = True
-#filePollInterval = 10
-
-# Solar interface
-#solarIp = "192.168.1.13:7380"
-#solarFileName = rootDir+"solar.json"
-
-# Loads interface
-#loadFileName = rootDir+"power.json"
-
-# Power interface
-###powerTbl = {"poolCleaner": 1500, "poolLight": 491, "spaLight": 276, "spaBlower": 712}
+debugEvent = False
+debugSched = False
 
