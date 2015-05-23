839584f0215b187458a981d3fbd9d4d0a3bc818c
Joe Buehl <joe@thebuehls.com>
2015-05-23 10:26:44 -0700

    Use MCP9803 sensor

--- haEdison.py
+++ haEdison.py
@@ -1,6 +1,6 @@
 from ha.HAClasses import *
 from ha.I2CCmdInterface import *
-from ha.TC74Interface import *
+from ha.MCP9803Interface import *
 from ha.tempInterface import *
 from ha.restServer import *
 
@@ -11,11 +11,11 @@ if __name__ == "__main__":
     # Interfaces
     stateChangeEvent = threading.Event()
     i2c1 = I2CCmdInterface("I2C1", bus=1, event=stateChangeEvent)
-    tc74 = TC74Interface("TC74", i2c1)
-    temp = TempInterface("Temp", tc74)
+    mcp9803 = MCP9803Interface("MCP9803", i2c1)
+    temp = TempInterface("Temp", mcp9803, sample=tempSample)
     
     # Temperature
-    resources.addRes(HASensor("edisonTemp", temp, 0x4b, group="Temperature", label="Edison temp", type="tempF"))
+    resources.addRes(HASensor("edisonTemp", temp, 0x49, group="Temperature", label="Edison temp", type="tempF"))
     
     # Schedules
 
