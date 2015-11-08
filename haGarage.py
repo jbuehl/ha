ad1a9f6ba5804ea6cd2845c6f2e40d27e6e7772a
Joe Buehl <joe@thebuehls.com>
2015-11-08 09:06:40 -0800

    Doors use interrupts

--- haGarage.py
+++ haGarage.py
@@ -5,15 +5,15 @@ from ha.TC74Interface import *
 from ha.tempInterface import *
 from ha.restServer import *
 from ha.restInterface import *
-#from ha.thingInterface import *
 
-#def frontLightSwitch(sensor, state):
-#    log(sensor.name, "state:", state)
-#    resources["frontLights"].setState(state)
+def doorInterrupt(sensor, state):
+    log(sensor.name, "state:", state)
+    stateChangeEvent.set()
     
 if __name__ == "__main__":
+    global stateChangeEvent
+
     # Resources
-    global resources
     resources = HACollection("resources")
 
     # Interfaces
@@ -23,27 +23,13 @@ if __name__ == "__main__":
     gpio1 = GPIOInterface("GPIO1", i2c1, addr=0x20, bank=1, inOut=0xff, config=[(GPIOInterface.IPOL+1, 0x75)])
     tc74 = TC74Interface("TC74", i2c1)
     temp = TempInterface("Temp", tc74, sample=10)
-#    thing = ThingInterface("Thing", event=stateChangeEvent)
     
     # Lights
-#    resources.addRes(HAControl("frontLights", gpio0, 0, type="light", group="Lights", label="Front lights"))
-#    frontLights = HAControl("backLights", HARestInterface("frontLights", service ="192.168.1.187:7378", cache=False, event=stateChangeEvent), "/resources/frontLight/state", type="light", group="Lights", label="Front lights")
     resources.addRes(HAControl("garageBackDoorLight", gpio0, 1, type="light", group="Lights", label="Garage back door light"))
-#    resources.addRes(HASensor("frontLightSwitch", gpio1, 0, type="light", group="Lights", label="Front light switch", interrupt=frontLightSwitch))
-#    resources.addRes(HAControl("bedroomLight", thing, type="dimmer", group="Lights", label="Bedroom light"))
-#    resources.addRes(HAControl("bathroomLight", thing, type="dimmer", group="Lights", label="Bathroom light"))
-#    bedroomLight = HAControl("bedroomLight", HARestInterface("bedroomLight", service ="192.168.1.153:7378", cache=False, event=stateChangeEvent), "/resources/bedroomLight/state", type="dimmer", group="Lights", label="Bedroom light")
-#    bathroomLight = HAControl("bathroomLight", HARestInterface("bathroomLight", service ="192.168.1.152:7378", cache=False, event=stateChangeEvent), "/resources/bathroomLight/state", type="dimmer", group="Lights", label="Bathroom light")
-#    backLights = HAControl("backLights", HARestInterface("backLights", service ="192.168.1.193:7378", cache=False, event=stateChangeEvent), "/resources/backLights/state", type="light", group="Lights", label="Back lights")
-#    resources.addRes(HAControl("testLight", gpio0, 7, type="light", group="Lights", label="TestOutput"))
-#    resources.addRes(HASensor("testSwitch", gpio1, 7, type="light", group="Lights", label="Test input"))
 
     # Doors
-    resources.addRes(HASensor("garageBackDoor", gpio1, 1, type="door", group="Doors", label="Garage Back"))
-    resources.addRes(HASensor("garageDoor", gpio1, 3, type="door", group="Doors", label="Garage Door"))
-
-    # Water
-#    resources.addRes(HAControl("recircPump", gpio0, 3, type="hotwater", group="Water", label="Hot water"))
+    resources.addRes(HASensor("garageBackDoor", gpio1, 1, type="door", group="Doors", label="Garage Back", interrupt=doorInterrupt))
+    resources.addRes(HASensor("garageDoor", gpio1, 3, type="door", group="Doors", label="Garage Door", interrupt=doorInterrupt))
 
     # Temperature
     resources.addRes(HASensor("garageTemp", temp, 0x4d, group="Temperature", label="Garage temp", type="tempF"))
