3d23ab741e114f7a802c26d61f7069fac77d59be
Joe Buehl <joe@thebuehls.com>
2016-05-14 17:14:42 -0700

    Initial commit

---
+++ haTest.py
@@ -0,0 +1,47 @@
+restIgnore = []
+
+import time
+from ha.HAClasses import *
+from ha.restInterface import *
+from ha.restServer import *
+from ha.restProxy import *
+from ha.timeInterface import *
+from haWeb import *
+
+stateChangeEvent = threading.Event()
+resourceLock = threading.Lock()
+
+if __name__ == "__main__":
+    # resources
+    try:
+        with open(rootDir+"aliases") as aliasFile:
+            aliases = json.load(aliasFile)
+    except:
+        aliases = {}
+    resources = HACollection("resources", aliases=aliases)
+
+    # time resources
+    timeInterface = TimeInterface("time")
+    resources.addRes(HASensor("theDayOfWeek", timeInterface, "%A", type="date", group="Time", label="Day of week"))
+    resources.addRes(HASensor("theDate", timeInterface, "%B %d %Y", type="date", group="Time", label="Date"))
+    resources.addRes(HASensor("theTimeAmPm", timeInterface, "%I:%M %p", type="time", group="Time", label="Time"))
+    resources.addRes(HASensor("sunrise", timeInterface, "sunrise", type="time", group="Time", label="Sunrise"))
+    resources.addRes(HASensor("sunset", timeInterface, "sunset", type="time", group="Time", label="Sunset"))
+    resources.addRes(HASensor("theDay", timeInterface, "%a %b %d %Y", type="date", label="Day"))
+    resources.addRes(HASensor("theTime", timeInterface, "%I:%M", type="time", label="Time"))
+    resources.addRes(HASensor("theAmPm", timeInterface, "%p", type="ampm", label="AmPm"))
+
+    # start the cache to listen for services on other servers
+    restIgnore.append(socket.gethostname()+":"+str(webRestPort))
+    restCache = RestProxy("restProxy", resources, restIgnore, stateChangeEvent, resourceLock)
+    restCache.start()
+    
+    # set up the web server
+    webInit(resources, restCache, stateChangeEvent, resourceLock)
+    
+    # start the REST server for this service
+    restServer = RestServer(resources, port=webRestPort, event=stateChangeEvent)
+    # restServer.start()
+    while True:
+        time.sleep(1)
+
