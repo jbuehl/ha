5bad4c816d21d2363ce6cb06f3cd4c870a2516b9
Joe Buehl <joe@thebuehls.com>
2016-08-29 07:27:30 -0700

    Clean up and document

--- restree.py
+++ restree.py
@@ -1,18 +1,29 @@
-from ha.restInterface import *
-import sys
+#!/usr/bin/env python
+# Print the resources and their attributes that are published in a REST service
 
-def resBranch(url, level):
-    node = rest.read(url)
+import sys
+from ha.restInterface import *
+
+# print out the resources in a branch of the tree
+def resBranch(path, level):
+    node = rest.read(path)
     print "    "*(level)+node["name"]
+    # print the resource attributes
     for attr in node.keys():
         if (attr != "name") and (attr != "resources"):
             print "    "*(level+1)+attr+": "+node[attr].__str__()
+    # if the resource is a collection, print its resources
     if "resources" in node.keys():
         for resource in node["resources"]:
-            resBranch(url+"/"+resource, level+1)
+            resBranch(path+"/"+resource, level+1)
         
 if __name__ == "__main__":
-    server = sys.argv[1]+":7378"
-    rest = HARestInterface("rest", server)
+    # get the host name
+    hostName = sys.argv[1]
+    # add the default port if it is not specified
+    if len(hostName.split(":")) == 1:
+        hostName += ":7378"
+    # instantiate the REST client
+    rest = HARestInterface("rest", service=hostName, cache=False)
     resBranch("/resources", 0)
 
