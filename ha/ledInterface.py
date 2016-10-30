479677eeb5fe7e020f5c1699cec318efd032dc77
Joe Buehl <joe@thebuehls.com>
2016-10-30 09:54:15 -0700

    Add flicker state to LEDs

---
+++ ha/ledInterface.py
@@ -0,0 +1,37 @@
+import random
+import time
+import threading
+from ha.HAClasses import *
+
+class LedInterface(HAInterface):
+    def __init__(self, name, interface=None, event=None):
+        HAInterface.__init__(self, name, interface=interface, event=event)
+        self.running = False
+        self.states = {}
+
+    def read(self, addr):
+        debug('debugLed', self.name, "read", addr)
+        return self.states[addr]
+
+    def write(self, addr, value):
+        debug('debugLed', self.name, "write", addr, value)
+        self.states[addr] = value
+        def flickerThread():
+            debug('debugLed', self.name, "starting", addr, value)
+            while self.running:
+               self.interface.write(addr, 1)
+               time.sleep(random.random()/10) 
+               self.interface.write(addr, 0)
+               time.sleep(random.random()/10) 
+            debug('debugLed', self.name, "ending", addr, value)
+        if (value == 0) or (value == 1):
+            if self.running:
+                self.running = False
+                time.sleep(.5)
+            self.interface.write(addr, value)
+        elif value == 2:
+            self.thread = threading.Thread(target=flickerThread)
+            self.running = True
+            self.thread.start()    
+
+
