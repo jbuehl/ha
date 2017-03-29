8a0d7f9f70cf6548f4193772156768e6959bc6d3
Joe Buehl <joe@thebuehls.com>
2017-03-29 16:19:38 -0700

    Move hvac stste to stateDir

--- haHvac.py
+++ haHvac.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     stateChangeEvent = threading.Event()
     nullInterface = HAInterface("NullInterface", event=stateChangeEvent)
     owfs = OWFSInterface("owfs", event=stateChangeEvent)
-    configData = FileInterface("configData", fileName=rootDir+"hvac.conf", event=stateChangeEvent)
+    configData = FileInterface("configData", fileName=stateDir+"hvac.conf", event=stateChangeEvent)
     i2c1 = I2CInterface("I2C1", bus=1, event=stateChangeEvent)
     gpio00 = GPIOInterface("GPIO00", i2c1, addr=0x20, bank=0)
     gpio10 = GPIOInterface("GPIO10", i2c1, addr=0x21, bank=0, inOut=0xff, config=[(GPIOInterface.IPOL, 0x00)])
