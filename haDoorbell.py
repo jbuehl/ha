479bf000babdee910a48d9e45b3b1b205e896ad3
Joe Buehl <joe@thebuehls.com>
2017-05-26 10:41:42 -0700

    Use rest proxy

--- haDoorbell.py
+++ haDoorbell.py
@@ -1,34 +1,33 @@
-doorbellService = "192.168.1.52:7378"
+doorbellService = "hostname:7378"
 doorbellSensor = "doorbellButton"
 doorbellState = 1
-doorbellSound = "doorbell2.wav"
+doorbellSound = "doorbell.wav"
 doorbellRepeat = 1
 
 doorbellDir = "/root/doorbell/"
 
 from ha import *
-from ha.interfaces.restInterface import *
+from ha.rest.restProxy import *
 
 if __name__ == "__main__":
-    # Resources
-    resources = Collection("resources")
-
-    # Interfaces
     stateChangeEvent = threading.Event()
-    restInterface = RestInterface("rest", None, service=doorbellService, event=stateChangeEvent)
-    
-    # Sensors
-    doorbell = Sensor(doorbellSensor, restInterface, "/resources/"+doorbellSensor+"/state")
-
+    resourceLock = threading.Lock()
+    resources = Collection("resources")
+    restCache = RestProxy("restProxy", resources, [], stateChangeEvent, resourceLock, watch=[doorbellService])
+    restCache.start()
     # Wait for events
     lastState = 0
     while True:
-        state = doorbell.getStateChange()
-        debug('debugDoorbell', "doorbellState", state, lastState)
-        if state != lastState:
-            if state == doorbellState:
-                for i in range(doorbellRepeat):
-                    os.system("aplay "+doorbellDir+doorbellSound)
-                state = 0
-        lastState = state
+        try:
+            state = resources[doorbellSensor].getStateChange()
+            debug('debugDoorbell', "state:", state, "lastState", lastState)
+            if state != lastState:
+                if state == doorbellState:
+                    for i in range(doorbellRepeat):
+                        os.system("aplay "+doorbellDir+doorbellSound)
+                    state = 0
+            lastState = state
+        except KeyError:
+            debug('debugDoorbell', "no doorbell")
+            time.sleep(1)
     
