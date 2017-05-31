28651fd327b37707daabc0ce54ac7d96ff5b2d70
Joe Buehl <joe@thebuehls.com>
2017-05-31 07:39:17 -0700

    rename apps

---
+++ hvacControl.py
@@ -0,0 +1,112 @@
+northHeatTempTargetDefault = 65
+northCoolTempTargetDefault = 75
+southHeatTempTargetDefault = 65
+southCoolTempTargetDefault = 75
+
+import threading
+from ha import *
+from ha.interfaces.gpioInterface import *
+from ha.interfaces.i2cInterface import *
+from ha.interfaces.owfsInterface import *
+from ha.interfaces.fileInterface import *
+from ha.interfaces.tempInterface import *
+from ha.controls.tempControl import *
+from ha.rest.restServer import *
+
+if __name__ == "__main__":
+    stateChangeEvent = threading.Event()
+
+    # Interfaces
+    nullInterface = Interface("nullInterface", event=stateChangeEvent)
+    owfs = OWFSInterface("owfs", event=stateChangeEvent)
+    configData = FileInterface("configData", fileName=stateDir+"hvac.conf", event=stateChangeEvent)
+    i2c1 = I2CInterface("i2c1", bus=1, event=stateChangeEvent)
+    gpio0 = GPIOInterface("gpio0", i2c1, addr=0x20, bank=0, inOut=0xff, config=[(GPIOInterface.IPOL, 0x00)])
+    gpio1 = GPIOInterface("gpio1", i2c1, addr=0x20, bank=1, inOut=0x00)
+
+    # Doors
+    frontDoor = Sensor("frontDoor", gpio0, 5, type="door", group="Doors", label="Front")
+    familyRoomDoor = Sensor("familyRoomDoor", gpio0, 6, type="door", group="Doors", label="Family room")
+    masterBedroomDoor = Sensor("masterBedroomDoor", gpio0, 7, type="door", group="Doors", label="Master bedroom")
+    houseDoors = SensorGroup("houseDoors", ["frontDoor", "familyRoomDoor", "masterBedroomDoor"], resources=resources, type="door", group="Doors", label="House doors")
+    
+    # persistent config data
+    northHeatTempTarget = Control("northHeatTempTarget", configData, "northHeatTempTarget", group="Hvac", label="North heat set", type="tempFControl")
+    northCoolTempTarget = Control("northCoolTempTarget", configData, "northCoolTempTarget", group="Hvac", label="North cool set", type="tempFControl")
+    southHeatTempTarget = Control("southHeatTempTarget", configData, "southHeatTempTarget", group="Hvac", label="South heat set", type="tempFControl")
+    southCoolTempTarget = Control("southCoolTempTarget", configData, "southCoolTempTarget", group="Hvac", label="South cool set", type="tempFControl")
+   
+    # Temperature sensors
+    masterBedroomTemp = Sensor("masterBedroomTemp", owfs, "28.175CDC060000", group="Temperature", label="Master bedroom temp", type="tempF")
+    kitchenTemp = Sensor("diningRoomTemp", owfs, "28.E4F6DB060000", group="Temperature", label="Dining room temp", type="tempF")
+    hallTemp = Sensor("hallTemp", owfs, "28.FA78DB060000", group="Temperature", label="Hall temp", type="tempF")
+    atticTemp = Sensor("atticTemp", owfs, "28.CC02DC060000", group="Temperature", label="Attic temp", type="tempF")
+    livingRoomTemp = Sensor("livingRoomTemp", owfs, "28.B9CA5F070000", group="Temperature", label="Living room temp", type="tempF")
+    familyRoomTemp = Sensor("familyRoomTemp", owfs, "28.7202DC060000", group="Temperature", label="Family room temp", type="tempF")
+    
+    # HVAC equipment
+    northHeat = Control("northHeat", gpio1, 4, group="Hvac", label="North heat")
+    southHeat = Control("southHeat", gpio1, 0, group="Hvac", label="South heat")
+    northCool = Control("northCool", gpio1, 5, group="Hvac", label="North cool")
+    southCool = Control("southCool", gpio1, 1, group="Hvac", label="South cool")
+    northFan  = Control("northFan",  gpio1, 6, group="Hvac", label="North fan")
+    southFan  = Control("southFan",  gpio1, 2, group="Hvac", label="South fan")
+
+    # Temp controls
+    northHeatControl = TempControl("northHeatControl", nullInterface, 
+                                    northHeat, masterBedroomTemp, northHeatTempTarget, masterBedroomDoor, unitType=0, 
+                                    group="Hvac", label="North heat control", type="heater")
+    northCoolControl = TempControl("northCoolControl", nullInterface, 
+                                    northCool, masterBedroomTemp, northCoolTempTarget, masterBedroomDoor, unitType=1, 
+                                    group="Hvac", label="North cool control", type="heater")
+    southHeatControl = TempControl("southHeatControl", nullInterface, 
+                                    southHeat, diningRoomTemp, southHeatTempTarget, familyRoomDoor, unitType=0, 
+                                    group="Hvac", label="South heat control", type="heater")
+    southCoolControl = TempControl("southCoolControl", nullInterface, 
+                                    southCool, diningRoomTemp, southCoolTempTarget, familyRoomDoor, unitType=1, 
+                                    group="Hvac", label="South cool control", type="heater")
+    
+    # Tasks
+    northHeatTempUpMorning = Task("northHeatTempUpMorning", SchedTime(hour=[6], minute=[0]), northHeatTempTarget, 69)
+    southHeatTempUpMorning = Task("southHeatTempUpMorning", SchedTime(hour=[6], minute=[0]), southHeatTempTarget, 70)
+    northHeatTempDownMorning = Task("northHeatTempDownMorning", SchedTime(hour=[8], minute=[0]), northHeatTempTarget, 66
+    southHeatTempDownMorning = Task("southHeatTempDownMorning", SchedTime(hour=[8], minute=[0]), southHeatTempTarget, 67)
+    northHeatTempDownEvening = Task("northHeatTempDownEvening", SchedTime(hour=[21], minute=[0]), northHeatTempTarget, 66)
+    southHeatTempDownEvening = Task("southHeatTempDownEvening", SchedTime(hour=[21], minute=[0]), southHeatTempTarget, 67)
+    
+    # Schedule
+    schedule = Schedule("schedule", tasks=[northHeatTempUpMorning, northHeatTempDownMorning, northHeatTempDownEvening,
+                                           southHeatTempUpMorning, southHeatTempDownMorning, southHeatTempDownEvening])
+
+    # Resources
+    resources = Collection("resources", resources=[frontDoor, familyRoomDoor, masterBedroomDoor, houseDoors,
+                                                   atticTemp, hallTemp, masterBedroomTemp, livingRoomTemp, familyRoomTemp, kitchenTemp,
+                                                   northHeat, northCool, northFan, northHeatTempTarget, northCoolTempTarget, northHeatControl, northCoolControl, 
+                                                   southHeat, southCool, southFan, southHeatTempTarget, southCoolTempTarget, southHeatControl, southCoolControl,
+                                                   northHeatTempUpMorning, northHeatTempDownMorning, northHeatTempDownEvening,
+                                                   southHeatTempUpMorning, southHeatTempDownMorning, southHeatTempDownEvening])
+    restServer = RestServer(resources, event=stateChangeEvent, label="Hvac")
+
+    # Start interfaces
+    configData.start()
+    if not northHeatTempTarget.getState():
+        northHeatTempTarget.setState(northHeatTempTargetDefault)
+    if not northCoolTempTarget.getState():
+        northCoolTempTarget.setState(northCoolTempTargetDefault)
+    if not southHeatTempTarget.getState():
+        southHeatTempTarget.setState(southHeatTempTargetDefault)
+    if not southCoolTempTarget.getState():
+        southCoolTempTarget.setState(southCoolTempTargetDefault)
+    # temporary
+    northHeatControl.setState(1)
+    southHeatControl.setState(1)
+    northCoolControl.setState(1)
+    southCoolControl.setState(1)
+#    northCool.setState(1)
+#    southCool.setState(1)
+
+    gpio0.start()
+    gpio1.start()
+    schedule.start()
+    restServer.start()
+
---
+++ sprinklerControl.py
@@ -0,0 +1,45 @@
+from ha import *
+from ha.interfaces.gpioInterface import *
+from ha.interfaces.i2cInterface import *
+from ha.rest.restServer import *
+
+if __name__ == "__main__":
+    stateChangeEvent = threading.Event()
+
+    # Interfaces
+    i2c1 = I2CInterface("i2c1", bus=1, event=stateChangeEvent)
+    gpioInterface = GPIOInterface("gpio", i2c1, addr=0x20, bank=1)
+
+    # Sprinklers
+    frontLawn = Control("frontLawn", gpioInterface, 4, group="Water", label="Front lawn valve") # yellow
+    garden = Control("garden", gpioInterface, 5, group="Water", label="Garden valve") # red
+    backLawn = Control("backLawn", gpioInterface, 6, group="Water", label="Back lawn valve") # green
+    sideBeds = Control("sideBeds", gpioInterface, 3, group="Water", label="Side beds valve") # red
+    backBeds = Control("backBeds", gpioInterface, 7, group="Water", label="Back beds valve") # blue
+
+    # Sequences
+    frontLawnSequence = Sequence("frontLawnSequence", [Cycle(frontLawn, 1200)], group="Water", label="Front lawn")
+    gardenSequence = Sequence("gardenSequence", [Cycle(garden, 600)], group="Water", label="Garden")
+    backLawnSequence = Sequence("backLawnSequence", [Cycle(backLawn, 1200)], group="Water", label="Back lawn")
+    sideBedSequence = Sequence("sideBedSequence", [Cycle(sideBeds, 600)], group="Water", label="Side beds")
+    backBedSequence = Sequence("backBedSequence", [Cycle(backBeds, 600)], group="Water", label="Back beds")
+
+    # Schedules
+    frontLawnTask = Task("frontLawnTask", SchedTime(hour=[21], minute=[00], weekday=[Mon, Wed, Fri], month=[May, Jun, Jul, Aug, Sep, Oct]), frontLawnSequence, 1, enabled=True)
+    gardenTask = Task("gardenTask", SchedTime(hour=[21], minute=[20], month=[May, Jun, Jul, Aug, Sep]), gardenSequence, 1, enabled=False)
+    backLawnTask = Task("backLawnTask", SchedTime(hour=[21], minute=[30], weekday=[Mon, Wed, Fri], month=[May, Jun, Jul, Aug, Sep, Oct]), backLawnSequence, 1, enabled=False)
+    sideBedTask = Task("sideBedTask", SchedTime(hour=[21], minute=[50], weekday=[Fri], month=[May, Jun, Jul, Aug, Sep, Oct]), sideBedSequence, 1, enabled=True)
+    backBedTask = Task("backBedTask", SchedTime(hour=[22], minute=[00], weekday=[Fri], month=[May, Jun, Jul, Aug, Sep, Oct]), backBedSequence, 1, enabled=False)
+    schedule = Schedule("schedule", tasks=[frontLawnTask, gardenTask, backLawnTask, sideBedTask, backBedTask])
+
+    # Resources
+    resources = Collection("resources", resources=[frontLawn, garden, backLawn, sideBeds, backBeds, 
+                                                   frontLawnSequence, gardenSequence, backLawnSequence, sideBedSequence, backBedSequence, 
+                                                   frontLawnTask, gardenTask, backLawnTask, sideBedTask, backBedTask])
+    restServer = RestServer(resources, event=stateChangeEvent, label="Sprinklers")
+
+    # Start interfaces
+    gpioInterface.start()
+    schedule.start()
+    restServer.start()
+
