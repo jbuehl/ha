af8641828e6db237347d104e8fd4c76383ff5f8d
Joe Buehl <joe@thebuehls.com>
2017-06-07 08:32:29 -0700

    Add lock to read

--- ha/interfaces/analogTempInterface.py
+++ ha/interfaces/analogTempInterface.py
@@ -1,4 +1,5 @@
 from ha import *
+import threading
 
 # analog temp sensor
 
@@ -6,11 +7,14 @@ class AnalogTempInterface(Interface):
     objectArgs = ["interface", "event"]
     def __init__(self, name, interface):
         Interface.__init__(self, name, interface)
+        self.lock = threading.Lock()
 
     def read(self, addr):
         debug('debugAnalogTemp', self.name, "read", addr)
         try:
-            volts = float(self.interface.read(addr))/1000 + .0005
+            with self.lock:
+                mVolts = self.interface.read(addr)
+            volts = float(mVolts)/1000 + .0005
             tempC = 28.25 * (2.52 - volts)
             tempF = tempC * 9 / 5 + 32
             debug('debugAnalogTemp', self.name, "volts", volts, "tempC", tempC, "tempF", tempF)
