bcce3d4b1fc1e16e976aa128c427009bfb3d958a
Joe Buehl <joe@thebuehls.com>
2017-07-03 15:12:31 -0700

    Utility to display REST beacon messages

---
+++ utilities/restbeacon.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# Print the messages that are sent to advertize REST services
+
+restBeaconPort = 4242
+
+import socket
+import json
+import time
+
+if __name__ == "__main__":
+    beaconSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    beaconSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    beaconSocket.bind(("", restBeaconPort))
+    while True:
+        (data, addr) = beaconSocket.recvfrom(8192)   # FIXME - need to handle arbitrarily large data
+        print "addr:", addr
+        print "data:", data
+        try:
+            serviceData = json.loads(data)
+            print "  hostname:", serviceData[0]
+            print "  port:", serviceData[1]
+            print "  resources:", serviceData[2]
+            print "  timestamp:", time.asctime(time.localtime(serviceData[3]))
+            print "  label:", serviceData[4]
+        except:
+            pass
+        print
+
