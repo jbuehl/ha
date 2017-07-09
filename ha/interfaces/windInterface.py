022001133d0ab95e086e1e217df07bfb333d1207
Joe Buehl <joe@thebuehls.com>
2017-07-09 07:53:42 -0700

    Debug messages

--- ha/interfaces/windInterface.py
+++ ha/interfaces/windInterface.py
@@ -1,4 +1,5 @@
 # Calculate wind speed and direction using the Peet Ultimeter Pro Anemometer
+timeOut = 10
 
 import time
 import threading
@@ -33,33 +34,51 @@ class WindInterface(Interface):
         self.anemometer.interrupt = speedInterrupt
         self.windVane = windVane        # wind vane contact closure sensor
         self.windVane.interrupt = dirInterrupt
-        debug("debugWind", "anemometer:", self.anemometer.name, "windVane:", self.windVane.name)
+        self.speed = 0.0
+        self.dir = 0.0
+        debug("debugWind", self.name, "anemometer:", self.anemometer.name, "windVane:", self.windVane.name)
 
     def read(self, addr):
+        curTime = time.time() - startTime
+        debug("debugWind", self.name, "read", "addr:", addr, "startTime:", startTime, "curTime:", curTime)
         if addr == "speed":
-            # calculate revolutions per second
-            try:
-                rps = 1.0 / speedTime
-            except ZeroDivisionError:
+            # if no data for timeOut seconds, there is no wind
+            if curTime > timeOut:
                 rps = 0.0
-            # approximate the wind speed in mph
-            if rps < .010:
                 mph = 0.0
-            elif rps < 3.229:   # 0.2 - 8.2 mph
-                mph = -0.1095 * rps**2 + 2.9318 * rps - 0.1412
-            elif rps < 54.362:  # 8.2 - 136.0 mph
-                mph = 0.0052 * rps**2 + 2.1980 * rps + 1.1091
-            else:               # 136.0 - 181.5 mph
-                mph = 0.1104 * rps**2 + 9.5685 * rps + 329.87
-            debug("debugWind", "mph:", mph, "rps:", rps)
-            return int(mph)
+            else:
+                # calculate revolutions per second
+                try:
+                    rps = 1.0 / speedTime
+                except ZeroDivisionError:
+                    rps = 0.0
+                # approximate the wind speed in mph
+                if rps < .010:
+                    mph = 0.0
+                elif rps < 3.229:   # 0.2 - 8.2 mph
+                    mph = -0.1095 * rps**2 + 2.9318 * rps - 0.1412
+                elif rps < 54.362:  # 8.2 - 136.0 mph
+                    mph = 0.0052 * rps**2 + 2.1980 * rps + 1.1091
+                else:               # 136.0 - 181.5 mph
+                    mph = 0.1104 * rps**2 + 9.5685 * rps + 329.87
+            debug("debugWind", self.name, "speedTime:", speedTime, "rps:", rps, "mph:", mph)
+            self.speed = mph
+            return int(mph + .5)
         elif addr == "dir":
-            try:
-                deg = (360 * dirTime / speedTime) % 360
-            except ZeroDivisionError:
+            # if no data for timeOut seconds, there is no wind
+            if curTime > timeOut:
                 deg = 0.0
+            else:
+                if self.speed != 0.0:
+                    try:
+                        deg = (360 * dirTime / speedTime) % 360
+                    except ZeroDivisionError:
+                        deg = 0.0
+                else:
+                    deg = 0.0
+            debug("debugWind", self.name, "dirTime:", dirTime, "speedTime:", speedTime, "deg:", deg)
+            self.dir = deg
+            return int(deg + .5)
         else:
-            deg = 0.0
-        debug("debugWind", "deg:", deg)
-        return int(deg)
+            return 0.0
 
