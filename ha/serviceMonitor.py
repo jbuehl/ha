86582c9399db88a65b186d3894dc002fbb40245a
Joe Buehl <joe@thebuehls.com>
2017-12-04 10:41:31 -0800

    Use resource lock

--- ha/serviceMonitor.py
+++ ha/serviceMonitor.py
@@ -10,22 +10,22 @@ def watchServices(resources, notifyNumbers, timeout=60):
     def serviceWatch():
         debug("debugServiceMonitor", "starting serviceWatch")
         while True:
-            for resource in resources:
-                if resource.split(".")[0] == "services":
-                    serviceName = resource.split(".")[1]
-#                    debug("debugServiceMonitor", serviceName, resources[resource].state)
-                    if resources[resource].state == 1:  # service is up
-                        serviceUpTimes[serviceName] = time.time()
-                    else:
-                        try:        # send notification is service was previously up
-                            if time.time() - serviceUpTimes[serviceName] > 20: #timeout:
-                                msg = "service "+resource.split(".")[1]+" is down"
-                                debug("debugServiceMonitor", msg)
-                                smsNotify(notifyNumbers, msg)
+            with resources.lock:
+                for resource in resources:
+                    if resource.split(".")[0] == "services":
+                        serviceName = resource.split(".")[1]
+                        debug("debugServiceMonitor", serviceName, resources[resource].state)
+                        if resources[resource].state == 1:  # service is up
+                            serviceUpTimes[serviceName] = time.time()
+                        else:
+                            try:        # send notification is service was previously up
+                                if time.time() - serviceUpTimes[serviceName] > 20: #timeout:
+                                    msg = "service "+serviceName+" is down"
+                                    debug("debugServiceMonitor", msg)
+                                    smsNotify(notifyNumbers, msg)
+                                    serviceUpTimes[serviceName] = float("inf")
+                            except KeyError:    # service is down at the start
                                 serviceUpTimes[serviceName] = float("inf")
-                        except KeyError:    # service is down at the start
-                            serviceUpTimes[serviceName] = float("inf")
-            debug("debugServiceMonitor", str(serviceUpTimes))
             time.sleep(serviceMonitorInterval)
                             
     serviceWatchThread = threading.Thread(target=serviceWatch)
