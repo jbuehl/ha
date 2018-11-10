3afd48bf1df8f84941f6191552e429c7b4edbe4d
Joe Buehl <joe@thebuehls.com>
2018-11-10 14:54:43 -0800

    Reorganize restInterface thread code

--- ha/interfaces/restInterface.py
+++ ha/interfaces/restInterface.py
@@ -17,6 +17,7 @@ class RestInterface(Interface):
         self.stateChange = stateChange      # server supports getStateChange
         self.hostname = socket.gethostname()
         self.enabled = False
+        self.readStateTimer = None
         debug('debugRest', self.name, "created", self.hostname, self.service) #, self.secure, self.cache, self.enabled)
         if self.secure:
             self.keyDir = keyDir
@@ -30,70 +31,76 @@ class RestInterface(Interface):
         self.enabled = True
         if self.cache:
             if self.stateChange:
-                # start the thread to update the cache when states change
-                def readStateChange():
-                    debug('debugRestStates', self.name, "readStateChange started")
-                    while self.enabled:
-                        states = self.readStates("/resources/states/stateChange")
-                        if states == {}:    # give up if there is an error
-                            break
-                        self.notify()
-                    debug('debugRestStates', self.name, "readStateChange terminated")
-                readStateChangeThread = threading.Thread(target=readStateChange)
+                readStateChangeThread = threading.Thread(target=self.readStateChange)
                 readStateChangeThread.start()
-            # start the thread to update the cache when a state change notification is received
-            def readStateNotify():
-                debug('debugRestStates', self.name, "readStateNotify started")
-                # open the socket to listen for state change notifications
-                debug('debugRest', self.name, "opening socket")
-                self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-                self.socket.bind(("", restStatePort))
-                # define a timer to disable the interface if the heartbeat times out
-                # can't use a socket timeout because multiple threads are using the same port
-                def readStateTimeout():
-                    debug('debugRestStateTimer', self.name, "timer expired")
-                    debug('debugRestDisable', self.name, "read state timeout")
-                    debug('debugRest', self.name, "disabled")
-                    self.enabled = False
-                readStateTimer = None
-                while self.enabled:
-                    if True: #try:
-                        # wait to receive a state change notification message
-                        (data, addr) = self.socket.recvfrom(8192)
-#                        debug('debugRestStates', self.name, "readStateNotify", "addr:", addr[0], "data:", data)
-                        msg = json.loads(data)
-                        if addr[0]+":"+str(msg["port"]) == self.service:   # is this from the correct service
-                            # this one is for us
-                            debug('debugRestStates', self.name, "readStateNotify", "addr:", addr[0], "data:", data)
-                            if readStateTimer:
-                                # cancel the timeout
-                                readStateTimer.cancel()
-                                debug('debugRestStateTimer', self.name, "timer cancelled", "read state")
-                            states = msg["state"]
-                            debug('debugRestStates', self.name, "readStateNotify", "states", states)
-                            # if still enabled, do it again
-                            if self.enabled:
-                                # update the states
-                                self.setStates(states)
-                                self.notify()
-                                # start the timer
-                                readStateTimer = threading.Timer(restStateTimeout, readStateTimeout)
-                                readStateTimer.start()
-                                debug('debugRestStateTimer', self.name, "timer started", restStateTimeout, "seconds")
-                    else: #except:
-                        debug('debugRest', self.name, "disabled")
-                        self.enabled = False
-                        break
-                # interface is no longer enabled, clean up
-                if readStateTimer:
-                    readStateTimer.cancel()
-                    debug('debugRestStateTimer', self.name, "timer cancelled", "service disabled")
-                self.stop()
-                debug('debugRestStates', self.name, "readStateNotify terminated")
-            readStateNotifyThread = threading.Thread(target=readStateNotify)
+            readStateNotifyThread = threading.Thread(target=self.readStateNotify)
             readStateNotifyThread.start()
 
+    # thread to update the cache when the stateChange request returns
+    def readStateChange(self):
+        debug('debugRestStates', self.name, "readStateChange started")
+        while self.enabled:
+            states = self.readStates("/resources/states/stateChange")
+            if states == {}:    # give up if there is an error
+                break
+            self.notify()
+        debug('debugRestStates', self.name, "readStateChange terminated")
+
+    # thread to update the cache when a state notification message is received
+    def readStateNotify(self):
+        debug('debugRestStates', self.name, "readStateNotify started")
+        # open the socket to listen for state change notifications
+        debug('debugRest', self.name, "opening socket")
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket.bind(("", restStatePort))
+        while self.enabled:
+            if True: #try:
+                # wait to receive a state change notification message
+                (data, addr) = self.socket.recvfrom(8192)
+#                        debug('debugRestStates', self.name, "readStateNotify", "addr:", addr[0], "data:", data)
+                msg = json.loads(data)
+                if addr[0]+":"+str(msg["port"]) == self.service:   # is this from the correct service
+                    # this one is for us
+                    debug('debugRestStates', self.name, "readStateNotify", "addr:", addr[0], "data:", data)
+                    self.cancelTimer("read state")
+                    states = msg["state"]
+                    debug('debugRestStates', self.name, "readStateNotify", "states", states)
+                    # if still enabled, do it again
+                    if self.enabled:
+                        # update the states
+                        self.setStates(states)
+                        self.notify()
+                        self.startTimer()
+            else: #except:
+                debug('debugRest', self.name, "disabled")
+                self.enabled = False
+                break
+        # interface is no longer enabled, clean up
+        self.cancelTimer("service disabled")
+        self.stop()
+        debug('debugRestStates', self.name, "readStateNotify terminated")
+
+    # define a timer to disable the interface if the heartbeat times out
+    # can't use a socket timeout because multiple threads are using the same port
+    def readStateTimeout(self):
+        debug('debugRestStateTimer', self.name, "timer expired")
+        debug('debugRestDisable', self.name, "read state timeout")
+        debug('debugRest', self.name, "disabled")
+        self.enabled = False
+
+    # start the read state timer
+    def startTimer(self):
+        self.readStateTimer = threading.Timer(restStateTimeout, self.readStateTimeout)
+        self.readStateTimer.start()
+        debug('debugRestStateTimer', self.name, "timer started", restStateTimeout, "seconds")
+
+    # cancel the read state timer
+    def cancelTimer(self, reason):
+        if self.readStateTimer:
+            self.readStateTimer.cancel()
+            debug('debugRestStateTimer', self.name, "timer cancelled", reason)
+                
     def stop(self):
         if self.enabled:
             self.enabled = False
