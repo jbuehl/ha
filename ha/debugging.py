e55dc2a2a27afb57db7728da9fee26f9318a66bb
Joe Buehl <joe@thebuehls.com>
2018-11-22 12:57:16 -0800

    Add debugTraceback()

--- ha/debugging.py
+++ ha/debugging.py
@@ -7,7 +7,17 @@ def debug(*args):
         if debugEnable:   # global debug flag enables debugging
             if globals()[args[0]]:  # only log if the specified debug variable is True
                 log(*args[1:])
-    except:
+    except NameError:
         pass
 
+# log a stack trace conditioned on a specified global variable
+def debugTraceback(debugType, debugName):
+    try:
+        if debugEnable:   # global debug flag enables debugging
+            if globals()[debugType]:
+                s = inspect.stack()
+                for f in s:
+                    log(debugName, f[1], f[2], f[3], f[4])
+    except NameError:
+        pass
 
