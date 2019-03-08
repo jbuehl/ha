b17c8d730591ea0b2535ea5bb58a280a416e965d
Joe Buehl <joe@thebuehls.com>
2019-03-08 09:13:49 -0800

    Verbal responses.  Numeric values

--- googleApp.py
+++ googleApp.py
@@ -1,7 +1,12 @@
+port = 4664
+haUrl = "http://localhost:81/cmd/"
+debug = True
+
 from BaseHTTPServer import HTTPServer
 from BaseHTTPServer import BaseHTTPRequestHandler
 import json
 import requests
+import syslog
 
 # convert a sensor name to a resource name
 def convertName(name):
@@ -11,8 +16,34 @@ def convertName(name):
         result += word.capitalize()
     return result
 
-port = 4664
-haUrl = "http://localhost:81/cmd/"
+# convert a value to a state that can be set
+stateDict = {"Increase": "%5E", "Decrease": "v",
+             "Raise": "Up", "Lower": "Down"}
+def convertValue(value):
+    try:
+        return stateDict[value.capitalize()]
+    except KeyError:
+        try:
+            return value.capitalize()
+        except AttributeError:
+            return state
+
+# convert a display state to a phrase that makes sense when spoken
+wordDict = {"f": "degrees",
+            "c": "degrees",
+            "in": "inches",
+            "gpm": "gallons per minute",
+            "kw": "kilowatts",
+            "kwh": "kilowatt hours",
+            "mwh": "megawatt hours",
+            "v": "volts"
+            }
+def convertState(state):
+    words = state.split()
+    try:
+        return words[0]+" "+wordDict[words[1].lower()]
+    except (IndexError, KeyError):
+        return state
 
 # Google agent fulfillment server
 class GoogleRequestHandler(BaseHTTPRequestHandler):
@@ -20,22 +51,39 @@ class GoogleRequestHandler(BaseHTTPRequestHandler):
         try:
             request = self.rfile.read(int(self.headers['Content-Length']))
             data = json.loads(request)
-            # pprint.pprint(data)
             action = data["queryResult"]["action"]
             parameters = data["queryResult"]["parameters"]
-            print action, parameters
+            if debug: syslog.syslog("action:"+action+" parameters:"+str(parameters))
             if action == "get":
-                url = haUrl+convertName(parameters["sensor"])
+                sensor = parameters["sensor"]
+                url = haUrl+convertName(sensor)
             elif action == "set":
-                url = haUrl+convertName(parameters["control"])+"/"+parameters["state"].capitalize()
+                control = parameters["control"]
+                sensor = control
+                if parameters["state"] != "":
+                    value = convertValue(parameters["state"])
+                elif parameters["number"] != "":
+                    value = str(int(parameters["number"]))
+                else:
+                    self.sendResponse("What do you want to set "+control+" to?")
+                    return
+                url = haUrl+convertName(control)+"/"+value
             elif action == "query":
-                url = haUrl+convertName(parameters["control"])
+                control = parameters["control"]
+                sensor = control
+                url = haUrl+convertName(control)
                 queryState = parameters["state"].capitalize()
-        except:
-            self.send_error(400) # error
+            else:
+                syslog.syslog("unrecognized action "+action)
+                self.sendResponse("I don't know how to "+action)
+                return
+        except Exception as ex:
+            syslog.syslog("Exception: "+str(ex))
+            self.sendResponse("There's a bug in your code "+str(ex))
             return
-        print url
+        if debug: syslog.syslog(url)
         try:
+            # execute the requested command and return the state
             reply = requests.get(url)
             state = reply.json()["state"]
             if action == "query":
@@ -43,31 +91,26 @@ class GoogleRequestHandler(BaseHTTPRequestHandler):
                     state = "Yes"
                 else:
                     state = "No"
-            print state
-            # build the webhook response
-            # https://developers.google.com/actions/build/json/dialogflow-webhook-json
-            response = {"payload": {"google": {"expectUserResponse": False,
-                                               "richResponse": {"items": [{"simpleResponse": {"textToSpeech": state}}]}
-                                               }}}
-            self.send_response(200) # success
-            self.send_header("Content-type", "application/json")
-            self.end_headers()
-            self.wfile.write(json.dumps(response))
-        except:
-            self.send_error(500) # error
+            if debug: syslog.syslog("state:"+state)
+            self.sendResponse(sensor+(" are " if sensor[-1]=="s" else " is ")+convertState(state))
+        except Exception as ex:
+            syslog.syslog("exception: "+str(ex))
+            self.sendResponse("Something didn't work "+str(ex))
 
-    def do_GET(self):
-        self.send_error(501)         # not implemented
-
-    def do_PUT(self):
-        self.send_error(501)         # not implemented
-
-    def do_DELETE(self):
-        self.send_error(501)         # not implemented
+    # build the webhook response
+    # https://developers.google.com/actions/build/json/dialogflow-webhook-json
+    def sendResponse(self, responseText):
+        response = {"payload": {"google": {"expectUserResponse": False,
+                                           "richResponse": {"items": [{"simpleResponse": {"textToSpeech": responseText}}]}
+                                           }}}
+        self.send_response(200) # success
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        self.wfile.write(json.dumps(response))
 
     # this suppresses logging from BaseHTTPServer
-    # def log_message(self, format, *args):
-    #     return
+    def log_message(self, format, *args):
+        return
 
 if __name__ == "__main__":
     googleServer = HTTPServer(('', port), GoogleRequestHandler)
