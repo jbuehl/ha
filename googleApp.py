606a1f70c5c6b2bd75b50aaee324a3320bc8c453
Joe Buehl <joe@thebuehls.com>
2019-03-12 20:44:42 -0700

    Add spoken control name to response

--- googleApp.py
+++ googleApp.py
@@ -1,12 +1,14 @@
 port = 4664
 haUrl = "http://localhost:81/cmd/"
 debug = True
+debugData = True
 
 from BaseHTTPServer import HTTPServer
 from BaseHTTPServer import BaseHTTPRequestHandler
 import json
 import requests
 import syslog
+import pprint
 
 # convert a sensor name to a resource name
 def convertName(name):
@@ -45,21 +47,33 @@ def convertState(state):
     except (IndexError, KeyError):
         return state
 
+# def findResource(data):
+#     return (resource, resourceName)
+
 # Google agent fulfillment server
 class GoogleRequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         try:
             request = self.rfile.read(int(self.headers['Content-Length']))
             data = json.loads(request)
+            if debugData: syslog.syslog(pprint.pformat(data))
             action = data["queryResult"]["action"]
             parameters = data["queryResult"]["parameters"]
             if debug: syslog.syslog("action:"+action+" parameters:"+str(parameters))
             if action == "get":
                 sensor = parameters["sensor"]
+                try:
+                    sensorName = data["queryResult"]["outputContexts"][0]["parameters"]["sensor.original"]
+                except KeyError:
+                    sensorName = sensor
                 url = haUrl+convertName(sensor)
             elif action == "set":
                 control = parameters["control"]
                 sensor = control
+                try:
+                    sensorName = data["queryResult"]["outputContexts"][0]["parameters"]["control.original"]
+                except KeyError:
+                    sensorName = sensor
                 if parameters["state"] != "":
                     value = convertValue(parameters["state"])
                 elif parameters["number"] != "":
@@ -71,6 +85,10 @@ class GoogleRequestHandler(BaseHTTPRequestHandler):
             elif action == "query":
                 control = parameters["control"]
                 sensor = control
+                try:
+                    sensorName = data["queryResult"]["outputContexts"][0]["parameters"]["control.original"]
+                except KeyError:
+                    sensorName = sensor
                 url = haUrl+convertName(control)
                 queryState = parameters["state"].capitalize()
             else:
@@ -88,11 +106,13 @@ class GoogleRequestHandler(BaseHTTPRequestHandler):
             state = reply.json()["state"]
             if action == "query":
                 if state == queryState:
-                    state = "Yes"
+                    answer = "Yes. "
                 else:
-                    state = "No"
+                    answer = "No. "
+            else:
+                answer = ""
             if debug: syslog.syslog("state:"+state)
-            self.sendResponse(sensor+(" are " if sensor[-1]=="s" else " is ")+convertState(state))
+            self.sendResponse(answer+"The "+sensorName+(" are " if sensor[-1]=="s" else " is ")+convertState(state))
         except Exception as ex:
             syslog.syslog("exception: "+str(ex))
             self.sendResponse("Something didn't work "+str(ex))
@@ -100,7 +120,7 @@ class GoogleRequestHandler(BaseHTTPRequestHandler):
     # build the webhook response
     # https://developers.google.com/actions/build/json/dialogflow-webhook-json
     def sendResponse(self, responseText):
-        response = {"payload": {"google": {"expectUserResponse": False,
+        response = {"payload": {"google": {"expectUserResponse": True,
                                            "richResponse": {"items": [{"simpleResponse": {"textToSpeech": responseText}}]}
                                            }}}
         self.send_response(200) # success
