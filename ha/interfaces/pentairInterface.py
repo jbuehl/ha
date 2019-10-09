8aa9196fa0d7ba7bd38c0663d0bc0bd551eb8571
Joe Buehl <joe@thebuehls.com>
2019-10-09 15:15:41 -0700

    Python3 conversion

--- ha/interfaces/pentairInterface.py
+++ ha/interfaces/pentairInterface.py
@@ -7,22 +7,22 @@ from ha import *
 from functools import reduce
 
 # ASCII constants
-NUL = '\x00'
-DLE = '\x10'
-STX = '\x02'
-ETX = '\x03'
-SOM = '\xa5'    # start of message
-FIL = '\xff'    # filler
-          
+NUL = b'\x00'
+DLE = b'\x10'
+STX = b'\x02'
+ETX = b'\x03'
+SOM = b'\xa5'    # start of message
+FIL = b'\xff'    # filler
+
 # addresses
-ctrlAddr  = '\x11'
+ctrlAddr  = b'\x11'
 
 # commands
-writeCmd   = '\x01'
-setCtrlCmd = '\x04'
-setModCmd  = '\x05'
-setRunCmd  = '\x06'
-sendCmd    = '\x07'
+writeCmd   = b'\x01'
+setCtrlCmd = b'\x04'
+setModCmd  = b'\x05'
+setRunCmd  = b'\x06'
+sendCmd    = b'\x07'
 
 # pump constants
 pa = 12.50
@@ -33,23 +33,22 @@ pc = 75.28
 # state of the pool pump
 ########################################################################################################
 class PentairInterface(Interface):
-    objectArgs = ["interface", "event"]
     # constructor
     def __init__(self, name, interface=None, event=None):
         Interface.__init__(self, name, interface=interface, event=event)
         # in theory there could be multiple pumps on an interface, but assume just one for now
         self.addr = pentairAddr
-        
+
         # The pump state consists of 4 values:
         # 0 - predefined pump speed 0..3
         # 1 - speed in rpm
         # 2 - power in watts
-        # 3 - flow in gpm 
-        
+        # 3 - flow in gpm
+
         self.state = [0, 0, 0, 0]
         self.manSpeed = 0           # the speed to which the pump has been manually set
         self.reqSpeed = -1          # the speed to which it is being requested to be set manually
-        
+
     def start(self):
         debug('debugPentairData', self.name, "starting pentair interface", self.interface.name)
         self.interface.start()
@@ -64,12 +63,12 @@ class PentairInterface(Interface):
             self.reqSpeed = value
 
     def readState(self):
-        self.sendMsg((self.addr, setCtrlCmd, '\xff'))
+        self.sendMsg(self.addr, setCtrlCmd, b'\xff')
         (dest, cmd, reply) = self.readMsg()
-        self.sendMsg((self.addr, sendCmd, ''))
+        self.sendMsg(self.addr, sendCmd, b'')
         (src, cmd, stat) = self.readMsg()
         status = struct.unpack("!BBBHHBBBBBBBB", stat)
-        self.sendMsg((self.addr, setCtrlCmd, '\x00'))
+        self.sendMsg(self.addr, setCtrlCmd, b'\x00')
         (src, cmd, reply) = self.readMsg()
         self.state[1] = status[4]
         self.state[2] = status[3]
@@ -87,53 +86,52 @@ class PentairInterface(Interface):
 
     def setSpeed(self, theSpeed):
         dat = struct.pack("!L",0x03210000+(theSpeed<<3))
-        self.sendMsg((self.addr, setCtrlCmd, '\xff'))
+        self.sendMsg(self.addr, setCtrlCmd, b'\xff')
         (dest, cmd, reply) = self.readMsg()
-        self.sendMsg((self.addr, writeCmd, dat))
+        self.sendMsg(self.addr, writeCmd, dat)
         (dest, cmd, reply) = self.readMsg()
-#            self.sendMsg((pumpAddr, sendCmd, ''))
+#            self.sendMsg(pumpAddr, sendCmd, b'')
 #            (src, cmd, stat) = self.readMsg()
 #            status = struct.unpack("!BBBHHBBBBBBBB", stat)
-        self.sendMsg((self.addr, setCtrlCmd, '\x00'))
+        self.sendMsg(self.addr, setCtrlCmd, b'\x00')
         (src, cmd, reply) = self.readMsg()
-    
+
     def readMsg(self):
         """ Read the next valid message from the serial port.
         Parses and returns the source address, command, and data as a tuple."""
-        while True:                                         
+        while True:
             pmsg = PentairMsg(iface=self.interface)
             # stop reading if a message with a valid checksum is read
             if checksum16(pmsg.msg[0:-2]) == pmsg.sum:
                 debug('debugPentairData', self.name, "-->", pmsg.printState())
                 return (pmsg.src, pmsg.cmd, pmsg.dat)
             else:
-                debug('debugPentairData', self.name, "-->", pmsg.printState(), 
+                debug('debugPentairData', self.name, "-->", pmsg.printState(),
                                   "*** bad checksum ***")
                 return (pmsg.src, pmsg.cmd, pmsg.dat)
 
-    def sendMsg(self, xxx_todo_changeme):
+    def sendMsg(self, dst, cmd, dat):
         """ Send a message.
-        The destination address, command, and data are specified as a tuple."""
-        (dst, cmd, dat) = xxx_todo_changeme
+        """
         pmsg = PentairMsg(dst=dst, src=ctrlAddr, cmd=cmd, dat=dat)
         debug('debugPentairData', self.name, "<--", pmsg.printState())
         self.interface.write(None, FIL+NUL+FIL+pmsg.msg)
 
 class PentairMsg(object):
-    def __init__(self, iface=None, dst="", src="", cmd="", dat=""):
+    def __init__(self, iface=None, dst=b"", src=b"", cmd=b"", dat=b""):
         if iface != None:    # read the message from the interface and parse it
-            self.msg = ""                  
-            while (self.msg != SOM):            # read until \xa5 
+            self.msg = b""
+            while (self.msg != SOM):            # read until \xa5
                 self.msg = iface.read(None, 1)
             self.msg += iface.read(None, 5)            # read header
-            dataLen = struct.unpack("!B", self.msg[-1])[0]
+            dataLen = self.msg[-1] # struct.unpack("!B", self.msg[-1])[0]
             self.msg += iface.read(None, dataLen+2)    # read data and checksum
-            self.som = self.msg[0]
-            self.sub = self.msg[1]
-            self.dst = self.msg[2]
-            self.src = self.msg[3]
-            self.cmd = self.msg[4]
-            self.len = self.msg[5]
+            self.som = self.msg[0:1]
+            self.sub = self.msg[1:2]
+            self.dst = self.msg[2:3]
+            self.src = self.msg[3:4]
+            self.cmd = self.msg[4:5]
+            self.len = self.msg[5:6]
             self.dat = self.msg[6:-2]
             self.sum = self.msg[-2:]
         else:           # construct the message from arguments
@@ -142,29 +140,29 @@ class PentairMsg(object):
             self.dst = dst
             self.src = src
             self.cmd = cmd
-            self.len = struct.pack("!B", len(dat))
+            self.len = bytes([len(dat)]) # struct.pack("!B", len(dat))
             self.dat = dat
             self.msg = self.som+self.sub+self.dst+self.src+cmd+self.len+self.dat
             self.sum = checksum16(self.msg)
             self.msg = self.msg+self.sum
 
     def printState(self):
-        return self.som.encode("hex")+self.sub.encode("hex")+" "+\
-                             self.dst.encode("hex")+" "+self.src.encode("hex")+" "+\
-                             self.cmd.encode("hex")+" "+self.len.encode("hex")+" "+\
-                             self.dat.encode("hex")+" "+\
-                             self.sum.encode("hex")
+        return self.som.hex()+self.sub.hex()+" "+\
+                             self.dst.hex()+" "+self.src.hex()+" "+\
+                             self.cmd.hex()+" "+self.len.hex()+" "+\
+                             self.dat.hex()+" "+\
+                             self.sum.hex()
 
 class PentairMsgThread(threading.Thread):
     """ Message handling thread.
 
     """
     def __init__(self, name, interface=None):
-        """ Initialize the thread."""        
+        """ Initialize the thread."""
         threading.Thread.__init__(self, target=self.doMsg)
         self.name = name
         self.interface = interface
-        
+
     def doMsg(self):
         """ Message handling loop.
         """
@@ -198,7 +196,5 @@ class PentairMsgThread(threading.Thread):
         debug('debugThread', self.name, "terminated")
 
 def checksum16(msg):
-    """ Compute the 16 bit checksum of a string of bytes."""                
-    return struct.pack("!H", reduce(lambda x,y:x+y, list(map(ord, msg))) % 16384)
-
-
+    """ Compute the 16 bit checksum of a string of bytes."""
+    return struct.pack("!H", reduce(lambda x,y:x+y, list(msg)) % 16384)
