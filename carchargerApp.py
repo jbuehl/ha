8bd70f327fdb011ca2a3c4b86f6563fd5d3590cc
Joe Buehl <joe@thebuehls.com>
2019-10-20 08:40:19 -0700

    Current threshold.  Start fileInterface.

--- carchargerApp.py
+++ carchargerApp.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
 
     # Sensors
     pilotVoltage = VoltageSensor("pilotVoltage", ads1015Interface, 0, group=["Car"], label="Pilot voltage", type="V", event=stateChangeEvent)
-    chargingCurrent = CurrentSensor("loads.carcharger.current", ads1015Interface, 1, currentFactor=10, threshold=10,
+    chargingCurrent = CurrentSensor("loads.carcharger.current", ads1015Interface, 1, currentFactor=10, threshold=.1,
                                 group=["Car", "Power", "Loads"], label="Car charger current", type="A", event=stateChangeEvent)
     chargingPower = PowerSensor("loads.carcharger.power", currentSensor=chargingCurrent, voltage=240,
                                 group=["Car", "Power", "Loads"], label="Car charger", type="KVA", event=stateChangeEvent)
@@ -49,5 +49,6 @@ if __name__ == "__main__":
 
     # Start interfaces
 #    carCharger.start()
+    fileInterface.start()
     schedule.start()
     restServer.start()
