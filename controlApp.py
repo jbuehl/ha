dd952f896ad97b5ce24949d18dd0744f7a3d473b
Joe Buehl <joe@thebuehls.com>
2019-11-23 15:01:16 -0800

    Change deck and trash lights to tp-link

--- controlApp.py
+++ controlApp.py
@@ -21,7 +21,9 @@ if __name__ == "__main__":
     configData = FileInterface("configData", fileName=stateDir+"control.state", event=stateChangeEvent, initialState=defaultConfig)
 
     # Controls
-    garageLight = Control("garageLight", tplinkInterface, "192.168.1.115", type="light", group=["Lights", "Garage"], label="Garage light")
+    garageLights = Control("garageLights", tplinkInterface, "192.168.1.115", type="light", group=["Lights", "Garage"], label="Garage lights")
+    deckLights = Control("deckLights", tplinkInterface, "192.168.1.128", type="light", group=["Lights", "Garage"], label="Deck lights")
+    trashLights = Control("trashLights", tplinkInterface, "192.168.1.133", type="light", group=["Lights", "Garage"], label="Trash lights")
     backHouseMusic = Control("backHouseMusic", tplinkInterface, "192.168.1.117", type="plug", group=["Plugs", "Backhouse"], label="Back house music")
 
     # start the cache to listen for services on other servers
@@ -96,7 +98,9 @@ if __name__ == "__main__":
                                                           [1, 0], [1, 0], [1, 0]],
                                                group="Modes", label="Vacation mode")
     # Resources
-    resources = Collection("resources", resources=[garageLight, backHouseMusic, porchLights, xmasLights, bedroomLights, outsideLights, guestMode, vacationMode])
+    resources = Collection("resources", resources=[garageLights, deckLights, trashLights,
+                                                   porchLights, xmasLights, bedroomLights, outsideLights,
+                                                   guestMode, vacationMode])
 
     # Light tasks
     resources.addRes(Task("bedroomLightsOnSunset", SchedTime(event="sunset"), "bedroomLights", 1, resources=resources, group="Lights"))
