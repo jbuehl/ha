67c8da7859c029c37bd6e38fec479dee8cfde471
Joe Buehl <joe@thebuehls.com>
2019-12-23 12:42:06 -0800

    Pattern changes

--- xmastreeApp.py
+++ xmastreeApp.py
@@ -1,6 +1,5 @@
 
 stringLength = 200
-darkLength = 0
 
 xmasTreeLabel = "Front yard lights"
 xmasTreePatternLabel = "Front yard light pattern"
@@ -16,26 +15,27 @@ from ha.controls.holidayLightControl import *
 from ha.rest.restServer import *
 
 # dictionary of patterns
-patterns = {"On": [Segment(darkLength, stringLength-darkLength, [color(255,255,255)], None)],
-            "Off": [Segment(darkLength, stringLength-darkLength, [off], None)],
-            "White": [Segment(darkLength, stringLength-darkLength, [white], None)],
-            "Pink": [Segment(darkLength, stringLength-darkLength, [pink], None)],
-            "Red": [Segment(darkLength, stringLength-darkLength, [red], None)],
-            "Orange": [Segment(darkLength, stringLength-darkLength, [orange], None)],
-            "Yellow": [Segment(darkLength, stringLength-darkLength, [yellow], None)],
-            "Green": [Segment(darkLength, stringLength-darkLength, [green], None)],
-            "Blue": [Segment(darkLength, stringLength-darkLength, [blue], None)],
-            "Purple": [Segment(darkLength, stringLength-darkLength, [purple], None)],
-            "Cyan": [Segment(darkLength, stringLength-darkLength, [cyan], None)],
-            "Magenta": [Segment(darkLength, stringLength-darkLength, [magenta], None)],
-            "Rust": [Segment(darkLength, stringLength-darkLength, [rust], None)],
-            "Indigo": [Segment(darkLength, stringLength-darkLength, [indigo], None)],
-            "Christmas": [Segment(darkLength, stringLength-darkLength-1, 3*[red]+3*[green], None), Segment(0, 1, [color(255,255,255)])],
-            "Christmas color": [Segment(darkLength, stringLength-darkLength-1, [red]+[green]+[purple]+[blue]+[yellow], None), Segment(0, 1, [color(255,255,255)])],
-            "Hanukkah": [Segment(darkLength, stringLength-darkLength, 7*[blue]+3*[white], None)],
-            "Spectrum": [Segment(darkLength, stringLength-darkLength, 10*[red]+10*[orange]+10*[yellow]+10*[green]+10*[cyan]+10*[blue]+10*[purple]+10*[magenta], CrawlAnimation(rate=10, direction=1))],
-            "Rabbit": [Segment(darkLength, stringLength-darkLength, 3*[rust]+3*[red]+3*[orange]+3*[yellow]+3*[green]+3*[cyan]+3*[blue]+3*[purple]+3*[indigo]+173*[off], CrawlAnimation(rate=1, direction=1))],
-            "Random": [Segment(darkLength, stringLength-darkLength, [off], RandomColorAnimation(rate=5))],
+patterns = {"On": [Segment(0, stringLength, [color(255,255,255)], None)],
+            "Off": [Segment(0, stringLength, [off], None)],
+            "White": [Segment(0, stringLength, [white], None)],
+            "Pink": [Segment(0, stringLength, [pink], None)],
+            "Red": [Segment(0, stringLength, [red], None)],
+            "Orange": [Segment(0, stringLength, [orange], None)],
+            "Yellow": [Segment(0, stringLength, [yellow], None)],
+            "Green": [Segment(0, stringLength, [green], None)],
+            "Blue": [Segment(0, stringLength, [blue], None)],
+            "Purple": [Segment(0, stringLength, [purple], None)],
+            "Cyan": [Segment(0, stringLength, [cyan], None)],
+            "Magenta": [Segment(0, stringLength, [magenta], None)],
+            "Rust": [Segment(0, stringLength, [rust], None)],
+            "Indigo": [Segment(0, stringLength, [indigo], None)],
+            "Christmas": [Segment(0, stringLength-1, 3*[red]+3*[green], None), Segment(0, 1, [color(255,255,255)])],
+            "Christmas color": [Segment(0, stringLength-1, [red]+[green]+[purple]+[blue]+[yellow], None), Segment(0, 1, [color(255,255,255)])],
+            "Hanukkah": [Segment(0, stringLength, 7*[blue]+3*[white], None)],
+            "Spectrum": [Segment(0, stringLength, 10*[red]+10*[orange]+10*[yellow]+10*[green]+10*[cyan]+10*[blue]+10*[purple]+10*[magenta], CrawlAnimation(rate=10, direction=1))],
+            "Rabbit": [Segment(0, stringLength, 3*[rust]+3*[red]+3*[orange]+3*[yellow]+3*[green]+3*[cyan]+3*[blue]+3*[purple]+3*[indigo]+173*[off], CrawlAnimation(rate=1, direction=1))],
+            "Random": [Segment(0, stringLength, [off], RandomColorAnimation(rate=5))],
+            "Sparkle": [Segment(0, stringLength, [color(255,255,255)], SparkleAnimation(rate=1, factor=.7))],
             }
 
 if __name__ == "__main__":
