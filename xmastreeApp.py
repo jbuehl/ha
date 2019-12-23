45d04192c5968fbf9247986343a462d698ea770a
Joe Buehl <joe@thebuehls.com>
2019-12-23 09:41:03 -0800

    Rename to front yard

--- xmastreeApp.py
+++ xmastreeApp.py
@@ -2,8 +2,8 @@
 stringLength = 200
 darkLength = 0
 
-xmasTreeLabel = "Living room lights"
-xmasTreePatternLabel = "Living room light pattern"
+xmasTreeLabel = "Front yard lights"
+xmasTreePatternLabel = "Front yard light pattern"
 
 defaultConfig = {
     "pattern": "Off",
@@ -33,23 +33,6 @@ patterns = {"On": [Segment(darkLength, stringLength-darkLength, [color(255,255,2
             "Christmas": [Segment(darkLength, stringLength-darkLength-1, 3*[red]+3*[green], None), Segment(0, 1, [color(255,255,255)])],
             "Christmas color": [Segment(darkLength, stringLength-darkLength-1, [red]+[green]+[purple]+[blue]+[yellow], None), Segment(0, 1, [color(255,255,255)])],
             "Hanukkah": [Segment(darkLength, stringLength-darkLength, 7*[blue]+3*[white], None)],
-            # "Halloween": [Segment(darkLength, stringLength-darkLength, 5*[orange]+3*[rust]+2*[purple], FlickerAnimation())],
-            # "Valentines day": [Segment(darkLength, stringLength-darkLength, 1*[white]+2*[pink]+5*[red]+2*[pink], None)],
-            # "St Patricks day": [Segment(darkLength, stringLength-darkLength, [green], None)],
-            # "May day": [Segment(darkLength, stringLength-darkLength, [red], None)],
-            # "Mardi gras": [Segment(darkLength, stringLength-darkLength, 3*[purple]+3*[yellow]+3*[green], SparkleAnimation(rate=5))],
-            # "Presidents day": [Segment(darkLength, stringLength-darkLength, 3*[red]+3*[white]+3*[blue], None)],
-            # "4th of July": [Segment(darkLength, stringLength-darkLength, 5*[red]+5*[white]+5*[blue], SparkleAnimation(rate=1))],
-            # "Bastille day": [Segment(darkLength, stringLength-darkLength, 10*[red]+10*[white]+10*[blue], None)],
-            # "Cinco de Mayo": [Segment(darkLength, stringLength-darkLength, 10*[green]+10*[white]+10*[red], None)],
-            # "Easter": [Segment(darkLength, stringLength-darkLength, [yellow]+[blue]+[green]+[cyan]+[magenta], None)],
-            # "Sweden day": [Segment(darkLength, stringLength-darkLength, 5*[blue]+5*[yellow], None)],
-            # "Canada day": [Segment(darkLength, stringLength-darkLength, 5*[red]+5*[white], None)],
-            # "Fall": [Segment(darkLength, stringLength-darkLength, 5*[red]+5*[orange]+5*[rust]+5*[orange], None)],
-            # "Gay pride": [Segment(darkLength, stringLength-darkLength, [pink]+[red]+[orange]+[yellow]+[green]+[blue]+[purple], SparkleAnimation(rate=3))],
-            # "Holi": [Segment(darkLength, stringLength-darkLength, [red]+[yellow]+[blue]+[green]+[orange]+[purple]+[pink]+[magenta], None)],
-            # "Columbus day": [Segment(darkLength, stringLength-darkLength, [green]+[white]+[red], None)],
-            # "MLK day": [Segment(darkLength, stringLength-darkLength, [white]+[red]+[yellow]+[rust], None)],
             "Spectrum": [Segment(darkLength, stringLength-darkLength, 10*[red]+10*[orange]+10*[yellow]+10*[green]+10*[cyan]+10*[blue]+10*[purple]+10*[magenta], CrawlAnimation(rate=10, direction=1))],
             "Rabbit": [Segment(darkLength, stringLength-darkLength, 3*[rust]+3*[red]+3*[orange]+3*[yellow]+3*[green]+3*[cyan]+3*[blue]+3*[purple]+3*[indigo]+173*[off], CrawlAnimation(rate=1, direction=1))],
             "Random": [Segment(darkLength, stringLength-darkLength, [off], RandomColorAnimation(rate=5))],
@@ -69,7 +52,7 @@ if __name__ == "__main__":
     # Resources
     xmasTree = HolidayLightControl("xmasTree", neopixelInterface, patterns, xmasTreePattern, type="light", group=["Lights", "Holiday"], label=xmasTreeLabel)
     resources = Collection("resources", resources=[xmasTree, xmasTreePattern])
-    restServer = RestServer("livingroom", resources, event=stateChangeEvent, label="Living room")
+    restServer = RestServer("frontyard", resources, event=stateChangeEvent, label="Front yard")
 
     # Start interfaces
     configData.start()
