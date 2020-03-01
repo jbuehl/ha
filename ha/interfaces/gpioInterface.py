8daa989b9439de4f83c08006847fddb3acfac4f4
Joe Buehl <joe@thebuehls.com>
2020-02-29 16:49:58 -0800

    Add invert option

--- ha/interfaces/gpioInterface.py
+++ ha/interfaces/gpioInterface.py
@@ -26,12 +26,13 @@ def interruptCallback(pin):
 
 # Interface to direct GPIO
 class GPIOInterface(Interface):
-    def __init__(self, name, interface=None, event=None, input=[], output=[]):
+    def __init__(self, name, interface=None, event=None, input=[], output=[], invert=False):
         Interface.__init__(self, name, interface=interface, event=event)
         global gpioInterface
         gpioInterface = self
         self.input = input
         self.output = output
+        self.invert = invert
 
     def start(self):
         gpio.setwarnings(False)
@@ -49,15 +50,22 @@ class GPIOInterface(Interface):
                 debug('debugGPIO', self.name, "setup", pin, gpio.OUT)
                 gpio.setup(pin, gpio.OUT)
                 debug('debugGPIO', self.name, "write", pin, 0)
-                gpio.output(pin, 0)
+                if self.invert:
+                    gpio.output(pin, 1)
+                else:
+                    gpio.output(pin, 0)
             else:
                 debug('debugGPIO', self.name, "ignoring", pin)
 
     def read(self, addr):
         value = gpio.input(addr)
+        if self.invert:
+            value = 1 - value
         debug('debugGPIO', self.name, "read", "addr:", addr, "value:", value)
         return value
 
     def write(self, addr, value):
         debug('debugGPIO', self.name, "write", "addr:", addr, "value:", value)
+        if self.invert:
+            value = 1 - value
         gpio.output(addr, value)
